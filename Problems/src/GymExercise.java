286652b69c326ef3fc4f0a2ee1a79afd38ad21ef
Ayush Rathore <ayusshrathore@gmail.com>
2022-07-23 20:38:54 +0530

    Add comment for problem

--- Problems/src/GymExercise.java
+++ Problems/src/GymExercise.java
@@ -1,6 +1,18 @@
 import java.util.*;
 
 public class Exercise {
+    
+     /**
+     * 
+     * Today you decided to go to the gym. You currently have energy equal to E
+     * units. There are N exercises in
+     * the gym. Each of these exercises drains Ai amount of energy from your body.
+     * 
+     * You feel tired if your energy reaches 0 or below. Calculate the minimum
+     * number of exercises you have
+     * to perform such that you become tired. Every unique exercise can only be
+     * performed at most 2 times as
+     */
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
