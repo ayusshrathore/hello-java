a09bc8a5dac042bbc09979cf2d14619769c4b4d2
aaaravv <aaravmishra619@gmail.com>
2022-09-03 23:48:20 +0530

    Uncomment driver code

--- DataStructures/AVLTrees/src/Main.java
+++ DataStructures/AVLTrees/src/Main.java
@@ -2,28 +2,28 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Tree tree = new Tree();
-        // tree.insert(3);
-        // tree.insert(2);
-        // tree.insert(6);
-        // tree.insert(4);
-        // tree.insert(9);
-        // tree.insert(1);
-        // tree.insert(10);
-        // tree.insert(11);
+        tree.insert(3);
+        tree.insert(2);
+        tree.insert(6);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(10);
+        tree.insert(11);
         /**
          * Check AVL rotations
          */
-        // tree.insert(10);
-        // tree.insert(20);
-        // tree.insert(30);
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
 
         /**
          * Check to see if a binary tree is balanced.
          */
-        // tree.insert(10);
-        // tree.insert(20);
-        // tree.insert(30);
-        // System.out.println(tree.isBalanced());
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
+        System.out.println(tree.isBalanced());
 
         /**
          * 2- Check to see if a binary tree is perfect. In a perfect binary tree, every
