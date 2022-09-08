583b2e3162dd1e3e399152a22647522367a94729
aaaravv <aaravmishra619@gmail.com>
2022-09-08 09:32:29 +0530

    Extend LinkedList class by few more methods
    mergeLinkedList, removeKthFromLast, addTwoNumbers, getIntersectionNode, reverseInKGroups, etc...

--- DataStructures/LinkList/src/LinkList.java
+++ DataStructures/LinkList/src/LinkList.java
@@ -1,4 +1,9 @@
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 public class LinkList {
     // Node class is something which our LinkList class will have internally. it's
@@ -254,7 +259,7 @@ public class LinkList {
         }
         if (b == null)
             System.out.println(a.data + "," + " " + a.next.data);
-        if (b == last)
+        else if (b == last)
             System.out.println(a.data);
     }
 
@@ -286,5 +291,241 @@ public class LinkList {
         list.last.next = node;
 
         return list;
+    }
+
+    public LinkList mergeLinkList(LinkList list1, LinkList list2) {
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+
+        if (list1.first.data > list2.first.data)
+            swap(list1, list2);
+
+        Node current1 = list1.first;
+        Node current2 = list2.first;
+        while (current1 != null && current2 != null) {
+            Node previous = null;
+            while (current1 != null && current1.data <= current2.data) {
+                previous = current1;
+                current1 = current1.next;
+            }
+            previous.next = current2;
+
+            Node temp = current1;
+            current1 = current2;
+            current2 = temp;
+        }
+        // 1 2 4
+        // 1 3 4
+
+        return list1;
+    }
+
+    public void removeKthFromLast(int k) {
+        // [1 -> 2 -> 3 -> 4 -> 5]
+        if (isEmpty())
+            throw new IllegalStateException();
+        var previous = first;
+        var diff = k - 1;
+        var current = previous;
+        var pre = first;
+        var count = 0;
+
+        while (diff > 0) {
+            if (current.next == null)
+                throw new IllegalArgumentException();
+            current = current.next;
+            count += 1;
+            diff--;
+        }
+        while (current != null) {
+            if (current.next == null)
+                pre.next = previous.next;
+
+            pre = previous;
+            previous = previous.next;
+            count += 1;
+            current = current.next;
+        }
+
+        if (count == k)
+            first = first.next;
+
+    }
+
+    public LinkList addTwoNumbers(LinkList list1, LinkList list2) {
+        var carry = 0;
+        var sum = 0;
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+
+        Node current1 = list1.first;
+        Node current2 = list2.first;
+        Node temp1 = list1.first;
+        Node temp2 = list2.first;
+        LinkList list = new LinkList();
+        while (temp2.next != null || temp1.next != null) {
+            if (temp1.next == null || temp2.next == null)
+                break;
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        if (temp1.next == null) {
+            while (temp2.next != null) {
+                Node zeros = new Node(0);
+                temp1.next = zeros;
+                temp2 = temp2.next;
+                temp1 = temp1.next;
+            }
+            temp1.next = null;
+        } else if (temp2.next == null) {
+            while (temp1.next != null) {
+                Node zeros = new Node(0);
+                temp2.next = zeros;
+                temp1 = temp1.next;
+                temp2 = temp2.next;
+            }
+            temp2.next = null;
+        }
+
+        while (current1 != null && current2 != null) {
+            sum = current1.data + current2.data + carry;
+            System.out.print(sum + " ");
+            if (sum >= 10) {
+                carry = sum / 10;
+                list.addLast(sum % 10);
+            } else {
+                list.addLast(sum);
+                carry = 0;
+            }
+            current1 = current1.next;
+            current2 = current2.next;
+        }
+        if (sum >= 10) {
+            Node newNode = new Node(carry);
+            list.last.next = newNode;
+        }
+        return list;
+    }
+
+    public LinkList alternativeAddTwoNumbers(LinkList l1, LinkList l2) {
+        Node current1 = l1.first;
+        Node current2 = l2.first;
+        int carry = 0;
+        LinkList list = new LinkList();
+        while (current1 != null || l2 != null || carry != 0) {
+            int sum = 0;
+            if (current1 != null) {
+                sum += current1.data;
+                current1 = current1.next;
+            }
+
+            if (current2 != null) {
+                sum += current2.data;
+                current2 = current2.next;
+            }
+
+            sum += carry;
+            carry = sum / 10;
+            list.addLast(sum % 10);
+
+        }
+        return list;
+    }
+
+    public Node getIntersectionNode(LinkList l1, LinkList l2) {
+        if (l1 == null || l2 == null)
+            return null;
+        var current1 = l1.first;
+        var current2 = l2.first;
+
+        while (current1.next != null || current2.next != null) {
+            if (current1 == current2)
+                return current1;
+            current1 = current1.next;
+            current2 = current2.next;
+        }
+        return null;
+    }
+
+    public LinkList reverseInKGroups(LinkList l, int k) {
+        var node = l.first;
+        LinkList result = new LinkList();
+        int size = 0;
+        while (node != null) {
+            node = node.next;
+            size++;
+        }
+        node = l.first;
+        int count = 0;
+        while (node != null) {
+            LinkList temp = new LinkList();
+            for (int i = 0; i < k; i++) {
+                if (node == null)
+                    break;
+
+                if (count < size / k)
+                    temp.addFirst(node.data);
+                else
+                    temp.addLast(node.data);
+
+                node = node.next;
+            }
+            var first = temp.first;
+            while (first != null) {
+                result.addLast(first.data);
+                first = first.next;
+            }
+            count++;
+        }
+        return result;
+    }
+
+    public boolean isPalindrome(LinkList l) {
+        Node head = l.first;
+        Node slow = head;
+        Node fast = head;
+
+        // fetch mid
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // reverse right half of the linklist
+        slow.next = reverseLinkedList(slow.next);
+
+        // making slow to point at right half since it is currently pointing middle
+        slow = slow.next;
+
+        // check for palindrome
+        while (slow != null) {
+            if (slow.data != head.data)
+                return false;
+            slow = slow.next;
+            head = head.next;
+        }
+        return true;
+    }
+
+    private Node reverseLinkedList(Node head) {
+        Node previous = null, next = null;
+
+        while (head != null) {
+            next = head.next;
+            head.next = previous;
+            previous = head;
+            head = next;
+        }
+        return previous;
+    }
+
+    public void swap(LinkList list1, LinkList list2) {
+        var temp = list1;
+        list1 = list2;
+        list2 = temp;
     }
 }
--- DataStructures/LinkList/src/Main.java
+++ DataStructures/LinkList/src/Main.java
@@ -53,5 +53,83 @@ public class Main {
         var list3 = list2.createWithLoop();
         System.out.println(list3.containsLoop());
 
+        LinkList list4 = new LinkList();
+        list4.addFirst(1);
+        list4.addFirst(2);
+        list4.addFirst(3);
+        list4.addFirst(4);
+        list4.addFirst(5);
+        list4.addFirst(6);
+        list4.fetchMiddle();
+
+        /**
+         * Merge two sorted Linked List
+         */
+        LinkList list5 = new LinkList();
+        LinkList list6 = new LinkList();
+        // list5.addLast(9);
+        // list5.addLast(10);
+        // list5.addLast(11);
+        // list5.addLast(12);
+        // list6.addLast(7);
+        // list6.addLast(8);
+        // list6.addLast(9);
+        // list6.addLast(10);
+        list5.addLast(1);
+        list5.addLast(2);
+        list5.addLast(4);
+        list6.addLast(1);
+        list6.addLast(3);
+        list6.addLast(4);
+        list5.mergeLinkList(list6, list5).print();
+
+        LinkList list7 = new LinkList();
+        list7.addLast(1);
+        list7.addLast(2);
+        list7.addLast(3);
+        list7.addLast(4);
+        list7.addLast(5);
+        // list7.removeKthFromLast(4);
+        System.out.println(list7.getKthFromLast(2));
+        list7.removeKthFromLast(5);
+        list7.print();
+
+        LinkList list8 = new LinkList();
+        LinkList list9 = new LinkList();
+        // list8.addLast(9);
+        // list8.addLast(9);
+        // list8.addLast(9);
+        // list8.addLast(9);
+        // list8.addLast(9);
+        // list8.addLast(9);
+        // list8.addLast(9);
+        // list9.addLast(9);
+        // list9.addLast(9);
+        // list9.addLast(9);
+        // list9.addLast(9);
+        list8.addLast(2);
+        list8.addLast(4);
+        list8.addLast(3);
+        list9.addLast(5);
+        list9.addLast(6);
+        list9.addLast(4);
+        list8.addTwoNumbers(list9, list8);
+        System.out.println(Arrays.toString(list8.addTwoNumbers(list9, list8).toArray()));
+
+        LinkList ls = new LinkList();
+        ls.addLast(1);
+        ls.addLast(2);
+        ls.addLast(1);
+        ls.addLast(1);
+        System.out.println(ls.isPalindrome(ls));
+
+        LinkList lst = new LinkList();
+        lst.addLast(1);
+        lst.addLast(2);
+        lst.addLast(3);
+        lst.addLast(4);
+        lst.addLast(5);
+        lst.reverseInKGroups(lst, 3).print();
+        ;
     }
 }
