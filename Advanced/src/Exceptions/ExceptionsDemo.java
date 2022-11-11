2333bba9ec2c088d61475775bf6c339cddbb6b29
aaaravv <aaravmishra619@gmail.com>
2022-11-11 21:40:17 +0530

    Catching multiple errors

--- Advanced/src/Exceptions/ExceptionsDemo.java
+++ Advanced/src/Exceptions/ExceptionsDemo.java
@@ -2,14 +2,22 @@ package Exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public  static void show(){
         try{
             var reader = new FileReader("file.txt");
-            System.out.println("File opened!");
+            var value = reader.read();
+            new SimpleDateFormat().parse("");
         } catch(FileNotFoundException ex){
             System.out.println(ex.getMessage());
+        }catch (IOException e){
+            System.out.println("Could not read the data.");
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
         }
     }
 
