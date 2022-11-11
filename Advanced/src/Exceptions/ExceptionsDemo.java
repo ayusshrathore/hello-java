9953ff47ef743720107f8f4b9a6c10c85a0e13bd
aaaravv <aaravmishra619@gmail.com>
2022-11-11 21:51:23 +0530

    Make use of finally block

--- Advanced/src/Exceptions/ExceptionsDemo.java
+++ Advanced/src/Exceptions/ExceptionsDemo.java
@@ -3,17 +3,23 @@ package Exceptions;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public  static void show(){
+        FileReader reader = null;
         try{
-            var reader = new FileReader("file.txt");
+            reader = new FileReader("file.txt");
             var value = reader.read();
-            new SimpleDateFormat().parse("");
-        } catch (IOException | ParseException e){
-            System.out.println("Could not read the data.");
+        }  catch (IOException e) {
+            throw new RuntimeException(e);
+        }finally {
+            if(reader != null ) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
         }
     }
 
