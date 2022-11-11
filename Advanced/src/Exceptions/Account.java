116d0e90238f82058b488409a67c8dd4c2582f55
aaaravv <aaravmishra619@gmail.com>
2022-11-11 23:12:38 +0530

    Chaining exceptions

--- Advanced/src/Exceptions/Account.java
+++ Advanced/src/Exceptions/Account.java
@@ -13,8 +13,12 @@ public class Account {
         // with data validation
         if(value <=0) throw new IOException();
     }
-    public void withdraw(float value) throws InsufficientFundsException {
-        if(value > balance)
-            throw new InsufficientFundsException();
+    public void withdraw(float value) throws AccountException {
+        if (value > balance) {
+            var fundsException = new InsufficientFundsException();
+            var accountException =  new AccountException();
+            accountException.initCause(fundsException);
+            throw accountException;
+        }
     }
 }
