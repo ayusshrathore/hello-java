ce3b2c9c9ead06fe742696f4a9bf76118a0b03ee
aaaravv <aaravmishra619@gmail.com>
2022-11-11 21:56:48 +0530

    Better external resource management with `try with resources`

--- Advanced/src/Exceptions/ExceptionsDemo.java
+++ Advanced/src/Exceptions/ExceptionsDemo.java
@@ -2,24 +2,19 @@ package Exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class ExceptionsDemo {
     public  static void show(){
-        FileReader reader = null;
-        try{
-            reader = new FileReader("file.txt");
+        // try with resources
+        try (
+                FileReader reader = new FileReader("file.txt");
+                var writer = new FileWriter("file2.txt");
+        ) {
             var value = reader.read();
-        }  catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
-        }finally {
-            if(reader != null ) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
         }
     }
 
