dd6cae8f1e814b118d9e554ae5e8c1f95c177440
aaaravv <aaravmishra619@gmail.com>
2022-11-13 09:58:29 +0530

    Implement anonymous inner class

--- Advanced/src/Lambdas/LambdasDemo.java
+++ Advanced/src/Lambdas/LambdasDemo.java
@@ -3,6 +3,14 @@ package Lambdas;
 public class LambdasDemo {
     public static void show(){
         greet(new ConsolePrinter());
+
+        // Anonymous inner class: referred to as inner class as it is used inside a method
+        greet(new Printer() {
+            @Override
+            public void print(String message) {
+                System.out.println(message);
+            }
+        });
     }
     public static void greet(Printer printer){
         printer.print("Hello World");
