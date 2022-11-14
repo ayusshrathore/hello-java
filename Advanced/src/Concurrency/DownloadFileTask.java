181ab68e2a19c2f5d705489fc51c3f01fa651539
aaaravv <aaravmishra619@gmail.com>
2022-11-14 17:29:35 +0530

    Confinement in action (preventing race condition)

--- Advanced/src/Concurrency/DownloadFileTask.java
+++ Advanced/src/Concurrency/DownloadFileTask.java
@@ -2,16 +2,31 @@ package Concurrency;
 
 // Runnable interface represents a task to be run on a thread
 public class DownloadFileTask implements Runnable{
+    private DownloadStatus status;
+
+    public  DownloadFileTask(){
+        this.status = new DownloadStatus();
+    };
+
     @Override
     public void run() {
         System.out.println("Downloading a file: " +  Thread.currentThread().getName());
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        // Simulating long task
+        for (int i = 0; i < 10_000; i++) {
+            if(Thread.currentThread().isInterrupted()) return; // checking for interrupt request
+            status.incrementTotalBytes();
         }
 
         System.out.println("Download complete: " + Thread.currentThread().getName());
+    }
+
+    public DownloadStatus getStatus() {
+        return status;
     }
 }
--- Advanced/src/Concurrency/ThreadDemo.java
+++ Advanced/src/Concurrency/ThreadDemo.java
@@ -1,12 +1,15 @@
 package Concurrency;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreadDemo {
     public static void show(){
         // Get the name of currently executing thread
         System.out.println(Thread.currentThread().getName());
 
         // Illustration for downloading 10 files from the server using 10 different threads in parallel
-        for(int i = 0; i < 10; i++) {
+        // for(int i = 0; i < 10; i++) {
             // It takes a instance of a class which implements Runnable interface
             Thread thread = new Thread(new DownloadFileTask()); // explicit thread for downloading a file
             thread.start(); // now after starting thread the code of downloading the file from DownloadFileTask
@@ -14,20 +17,53 @@ public class ThreadDemo {
             // actually have to kill any
 
             // Joining thread (for e.g. scanning for viruses) in order to wait for all threads to download file
-            try {
-                thread.join(); // by this we tell the current thread to wait for completion of another thread
+            //try {
+            //    thread.join(); // by this we tell the current thread to wait for completion of another thread
                 // also for a desktop or mobile phone main thread is responsible for handling mouse clicks and keystrokes
                 // while it waiting for another thread it will not respond to such events anymore, so UI will freeze
                 // & user won't be able to resize or move application anyhow
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("File is ready to be scanned.");
-        }
+            //} catch (InterruptedException e) {
+            //    throw new RuntimeException(e);
+            //}
+            //System.out.println("File is ready to be scanned.");
+        //}
 
         // If we started downloading large number of files concurrently than our actual thread count than jvm has a thread
         // scheduler job of this scheduler is to decide what threads to run for how long, by giving a slice to cpu to
         // each thread and switching between them, this happens so quickly that it give us illusion of this happening
         // in parallel
+
+        // Interrupting a thread
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) { // while thread is sleeping and is interrupted it raise an exception
+            throw new RuntimeException(e);
+        }
+        // cancelling current thread
+        thread.interrupt(); // it just sends the interrupt request to the thread doesn't really force it to stop
+
+        // Issues with concurrency (Racing Condition)
+        List<Thread> threads = new ArrayList<>();
+        List<DownloadFileTask> tasks = new ArrayList<>();
+        for (int i = 0; i <10; i++) {
+            var task = new DownloadFileTask();
+            tasks.add(task);
+
+            Thread thread1 = new Thread(task);
+            thread1.start();
+            threads.add(thread1);
+        }
+        // waiting all download threads to finish
+        for(var thread2 : threads) {
+            try {
+                thread2.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        var totalBytes = tasks.stream().map(task -> task.getStatus().getTotalBytes())
+                .reduce(Integer::sum);
+        System.out.println(totalBytes); // preventing race condition using confinement by using different
+        // status objects and combining them later to get the result
     }
 }
