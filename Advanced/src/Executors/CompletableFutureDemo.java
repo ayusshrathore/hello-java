56bdec29a1f58327a91e5e963a86622ebe8484d4
aaaravv <aaravmishra619@gmail.com>
2022-11-15 18:13:39 +0530

    CompletableFuture with a `Supplier`

--- Advanced/src/Executors/CompletableFutureDemo.java
+++ Advanced/src/Executors/CompletableFutureDemo.java
@@ -1,13 +1,23 @@
 package Executors;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
 
 public class CompletableFutureDemo {
     public static void show(){
         // Run a code in asynchronous or a non-blocking fashion
-        Runnable task = () -> System.out.println("A");
-        var future = CompletableFuture.runAsync(task); // with CompletableFuture we don't really
+        Supplier<Integer> task = () -> 1;
+        // If we want a return value we should make use of Supplier interface and pass a supplier instead of
+        // a runnable to CompletableFuture
+        var future = CompletableFuture.supplyAsync(task);
+        // with CompletableFuture we don't really
         // need to create an executor, submit a task to it and finally shutdown CompletableFuture dones
         // this own its own
+        try {
+            var result = future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
