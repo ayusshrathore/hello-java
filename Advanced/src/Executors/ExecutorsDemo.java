b16c7e8d0acccfdd107c4a6e9f2464d7e8d83d3d
aaaravv <aaravmishra619@gmail.com>
2022-11-15 17:47:11 +0530

    Callables & futures demo

--- Advanced/src/Executors/ExecutorsDemo.java
+++ Advanced/src/Executors/ExecutorsDemo.java
@@ -1,5 +1,6 @@
 package Executors;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
 public class ExecutorsDemo {
@@ -7,9 +8,23 @@ public class ExecutorsDemo {
         var executor = Executors.newFixedThreadPool(2);
 
         try {
-            executor.submit(() ->
-                    System.out.println(Thread.currentThread().getName()));
-        }finally {
+            // callable which is a just like a runnable which returns a generic type value
+            // Its type belong to Future interface which represents future result of an operation
+            // which will be calculated in future
+            var future = executor.submit(() ->{
+                LongTask.simulate();
+                return 1;
+            });
+
+            // Submit Method won't wait for LongTask & will jump here
+            System.out.println("Do more work");
+            var result = future.get(); // this method will block the current thread until result of
+            // this operation is ready
+            System.out.println(result);
+
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
             executor.shutdown(); // Since the thread pool doesn't shut down on its own we do need to
             // explicitly shutdown to terminate our program, it doesn't terminate the current task but
             // stop receiving new ones & wait for them to finish in order to force shutdown tasks we can
