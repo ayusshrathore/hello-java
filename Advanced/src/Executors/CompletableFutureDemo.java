6cd3395efd9bf81359b49788905793a9871e3c2a
aaaravv <aaravmishra619@gmail.com>
2022-11-15 21:27:22 +0530

    Transforming a `CompletableFutures`

--- Advanced/src/Executors/CompletableFutureDemo.java
+++ Advanced/src/Executors/CompletableFutureDemo.java
@@ -5,27 +5,24 @@ import java.util.concurrent.ExecutionException;
 import java.util.function.Supplier;
 
 public class CompletableFutureDemo {
+    public static int toFahrenheit(int celsius){
+        return (int) (celsius * 1.8) + 32;
+    }
     public static void show(){
         // Run a code in asynchronous or a non-blocking fashion
-        Supplier<Integer> task = () -> 1;
+        Supplier<Integer> task = () -> 20;
         // If we want a return value we should make use of Supplier interface and pass a supplier instead of
         // a runnable to CompletableFuture
-        var future = CompletableFuture.supplyAsync(task);
+        var future = CompletableFuture.supplyAsync(task); // build a completable future
         // Running code on completion of asynchronous task
         // .thenRunAsync() will execute underlying task on a different thread
         // whereas .thenRun() will execute it on main thread
         // .thenAccept() takes an consumer object
-        future.thenAcceptAsync((result) -> {
-            System.out.println(Thread.currentThread().getName());
-            System.out.println(result);
-        });
+            var result = future.thenApply(CompletableFutureDemo::toFahrenheit); // transform a completable future
+            result.thenAccept(System.out::println); // accept or consume a completable future
+
         // with CompletableFuture we don't really
-        // need to create an executor, submit a task to it and finally shutdown CompletableFuture dones
+        // need to create an executor, submit a task to it and finally shutdown CompletableFuture does
         // this own its own
-        try {
-            var result = future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
     }
 }
