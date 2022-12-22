539eb472bb668d3a2999db133a77d6edca16713f
aaaravv <aaravmishra619@gmail.com>
2022-12-22 17:48:29 +0530

    chore: WeightedUndirected Graph implementation

---
+++ DataStructures/UndirectedGraphs/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // weighted undirected graph
+        var graph = new WeightedGraph();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("A", "B", 2);
+        graph.addEdge("A", "C", 3);
+        graph.print();
+    }
+}
---
+++ DataStructures/UndirectedGraphs/src/WeightedGraph.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WeightedGraph {
+    private class Node{
+        String label;
+
+        public Node(String label){
+            this.label = label;
+        }
+
+        @Override
+        public String toString(){
+            return label;
+        }
+    }
+
+    private class Edge{
+        private Node from;
+        private Node to;
+        private int weight;
+
+        public Edge(Node from, Node to, int weight){
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+
+        @Override
+        public String toString(){
+            return from + "->" + to;
+        }
+    }
+
+    private Map<String, Node> nodes = new HashMap<>();
+    private Map<Node, List<Edge>> adjacencyList= new HashMap<>();
+
+    public void addNode(String label){
+        var node = new Node(label);
+        nodes.putIfAbsent(label, node);
+        adjacencyList.putIfAbsent(node, new ArrayList<>()); // adding node to adjacencyList with empty ArrayList to
+    }
+
+    public void addEdge(String from, String to, int weight){
+        var fromNode = nodes.get(from);
+        if(fromNode == null) throw new IllegalArgumentException();
+
+        var toNode = nodes.get(to);
+        if(toNode == null) throw new IllegalArgumentException();
+
+        // weighted undirected graph
+        adjacencyList.get(fromNode).add(new Edge(fromNode, toNode, weight));
+        adjacencyList.get(toNode).add(new Edge(toNode, fromNode, weight));
+    }
+
+    public void print(){
+        for(var source: adjacencyList.keySet()){
+            var targets = adjacencyList.get(source);
+            if(!targets.isEmpty())
+                System.out.println(source + " is connected to " + targets);
+        }
+    }
+}
