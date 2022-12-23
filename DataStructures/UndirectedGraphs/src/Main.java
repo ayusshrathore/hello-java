844825e459daa9033b6c9e81102e120a8378b327
aaaravv <aaravmishra619@gmail.com>
2022-12-23 09:45:36 +0530

    chore: Implement `hasCycle()` method
    to determine cycles in weighted undirected graphs

--- DataStructures/UndirectedGraphs/src/Main.java
+++ DataStructures/UndirectedGraphs/src/Main.java
@@ -2,13 +2,22 @@ public class Main {
     public static void main(String[] args) {
         // weighted undirected graph
         var graph = new WeightedGraph();
+//        graph.addNode("A");
+//        graph.addNode("B");
+//        graph.addNode("C");
+//        graph.addEdge("A", "B", 2);
+//        graph.addEdge("B", "C", 3);
+//        graph.addEdge("A", "C", 7);
+//        graph.print();
+//        System.out.println(graph.getShortestPath("A", "C"));
+
+        // detect cycles in undirected graphs
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
-        graph.addEdge("A", "B", 2);
-        graph.addEdge("B", "C", 3);
-        graph.addEdge("A", "C", 7);
-        graph.print();
-        System.out.println(graph.getShortestPath("A", "C"));
+        graph.addEdge("A", "B", 0);
+        graph.addEdge("A", "C", 0);
+        graph.addEdge("C", "A", 0);
+        System.out.println(graph.hasCycle());
     }
 }
